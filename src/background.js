103970a2937b18024364a25a64a5879c8cc7c045
Chris Gonzales <chrisg32@users.noreply.github.com>
2022-03-25 15:25:39 -0500

    Initial commit.

---
+++ src/background.js
@@ -0,0 +1,14 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+		chrome.windows.create({ tabId : tab.id, type: 'popup' });
+	});
+
+chrome.runtime.onMessage.addListener(function(request) {
+	if(request.scheme === "dark"){
+		chrome.browserAction.setIcon({
+			path: { "16": "icons/icon16dark.png",
+			"48": "icons/icon48dark.png",
+			"96": "icons/icon96dark.png",
+		   "128": "icons/icon128dark.png" }
+		});
+	}
+});
